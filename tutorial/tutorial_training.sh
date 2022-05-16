5948fd262361697062598a9e9d978ef83b60a427
Anna Weber <wbr@zurich.ibm.com>
2022-05-16 12:04:02 +0200

    remove tutorial notebook

--- tutorial/tutorial_training.sh
+++ tutorial/tutorial_training.sh
@@ -1,20 +1,9 @@
 python3 ../scripts/flexible_training.py \
-data/fold0/train_small.csv \
-data/fold0/test_small.csv  \
-data/tcr.csv \
-data/epitopes.csv \
+data/train_small.csv \
+data/test_small.csv  \
+data/tcr_full.csv \
+data/epitopes.smi \
 trained_model \
 data/params_small.json \
 tutorial_setting \
 bimodal_mca
-
-#python3 ../scripts/semifrozen_finetuning.py \
-#data/fold0/train.csv \
-#data/fold0/test.csv  \
-#data/tcr.csv \
-#data/epitopes.smi \
-#'/Users/wbr/Library/CloudStorage/Box-Box/Molecular_SysBio/data/paccmann/TITAN/public/trained_model' \
-#trained_model \
-#finetuned_model \
-#../params/params_finetuning.json \
-#bimodal_mca
